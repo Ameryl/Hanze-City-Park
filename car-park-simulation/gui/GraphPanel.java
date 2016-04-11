eeeb68bcc69b08e8efaa50a9a472ce04fe83ff9d
BOB <piemels@mailnesia.com>
2016-04-11 23:04:27 +0200

    Added library and gui

---
+++ car-park-simulation/gui/GraphPanel.java
@@ -0,0 +1,41 @@
+package gui;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+
+
+
+public class GraphPanel extends JPanel {
+
+    private int width = 800;
+    private int heigth = 400;
+    private int padding = 25;
+    private int labelPadding = 25;
+    private Color lineColor = new Color(44, 102, 230, 180);
+    private Color pointColor = new Color(100, 100, 100, 180);
+    private Color gridColor = new Color(200, 200, 200, 200);
+    private static final Stroke GRAPH_STROKE = new BasicStroke(2f);
+    private int pointWidth = 4;
+    private int numberYDivisions = 10;
+    private List<Double> scores;
+
+    public GraphPanel(List<Double> scores) {
+        this.scores = scores;
+    }
+
+
+}
