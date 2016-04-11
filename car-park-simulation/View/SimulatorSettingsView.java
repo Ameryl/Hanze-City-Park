12c3338b0edcfb4c400b480a80a5ab2d6d5a3a48
BOB <piemels@mailnesia.com>
2016-04-11 14:54:10 +0200

    Addes simulator settings view. To make a simulation with custom values.

---
+++ car-park-simulation/View/SimulatorSettingsView.java
@@ -0,0 +1,18 @@
+package View;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+import Logic.*;
+import Runner.Simulator;
+
+
+
+public class SimulatorSettingsView extends JFrame {
+    private JTextField minuteValue;
+    private JTextField hourValue;
+    private JTextField dayValue;
+    private JTextField stepsValue;
+
+}
